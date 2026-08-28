a0dc65c4294ec1e79f544eb7055321ef9335a827
Vlad <forntend.freak616@gmail.com>
2026-08-28 16:30:49 +0300

    feat: добавил cors

--- server.ts
+++ server.ts
@@ -1,9 +1,17 @@
 import express, { type Request, type Response } from "express";
+import cors from "cors";
 import { PORT } from "./constants.js";
 /* import plansRouter from "./routes/plans.js"; */
 import servicesRouter from "./routes/services.js";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+    }),
+);
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
